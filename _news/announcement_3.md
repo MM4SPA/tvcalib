54eef2827222a0233a39f6fac068637c148ba54a
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-03-31 09:04:58 +0000

    replaced article with inproceedings [ci skip]

--- _news/announcement_3.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 2022-08-30 05:00:00-0000
-inline: true
----
-
-<a href="https://arxiv.org/abs/2207.11709">Preprint</a> is available as well as source code to reproduce results.
--- assets/js/smooth_scroll.js
+++ assets/js/smooth_scroll.js
@@ -1,10 +1,1 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+document.querySelectorAll('a[href^="#"]').forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})});
